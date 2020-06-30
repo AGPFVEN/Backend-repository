3ac52d7123a5e892b57627cd795cdbf68ee18a41
AGPFVEN <50179663+AGPFVEN@users.noreply.github.com>
2020-06-30 03:16:09 +0200

    showing the data

--- AGPFVEN/php_crud_mysql/db.php
+++ AGPFVEN/php_crud_mysql/db.php
@@ -2,7 +2,7 @@
 
 session_start();              #Así se inicia una sesion que sirve para guardar datos atraves de scripts, updates, etc
 
-$conection = mysqli_connect(  #Conectando a base de datos
+$connection = mysqli_connect( #Conectando a base de datos
     'localhost',              #URL, dominio, pero en este caso es local
     'root',                   #Usuario, este es el que crea xampp by default 
     '',                       #Contraseña, xampp te crea un usuario sin contraseña
--- AGPFVEN/php_crud_mysql/index.php
+++ AGPFVEN/php_crud_mysql/index.php
@@ -5,16 +5,23 @@
 <div class="container p-4">
     <div class="row">
         <div class="col-md-4">
+
             <!--Comprobar si el mensaje exiate con php, el mensaje va a existir si hemos hecho un quary antes-->
-            <?php if(isset($_SESSION['message'])) { ?>
-            <!--Así puedes meter código html entre php-->
-            <div class="alert alert-<?=$_SESSION['message_type'];?> alert-dismissible fade show" role="alert">
-                <?= $_SESSION['message'] ?>
-                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <?php session_unset(); } ?>
+            <?php if(isset($_SESSION['message'])) 
+            { ?>
+
+                <!--Así puedes meter código html entre php-->
+                <div class="alert alert-<?=$_SESSION['message_type'];?> alert-dismissible fade show" role="alert">
+                    <?= $_SESSION['message'] ?>
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+
+                <!-- Borra los datos que hay dentro de session -->
+                <?php session_unset(); 
+            } ?>
+
             <div class="card card-body">
                 <form action="save_task.php" method="POST">
                     <div class="form-group">
@@ -29,16 +36,32 @@
             </div>
         </div>
         <div class="com-md-8">
-        <table class="table table-bordered">
-        <thead>
-        <tr>
-        <th> Title </th>
-        <th> Description </th>
-        <th> Created At</th>
-        <th> Actions</th>
-        </tr>
-        </thead>        
-        </table>
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th> Title </th>
+                        <th> Description </th>
+                        <th> Created At</th>
+                        <th> Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    $query = "SELECT * FROM task";                                       #Seleccionar todo dentro de la base de datos
+                    $result_tasks = mysqli_query($connection, $query);
+
+                    while($row = mysqli_fetch_array($result_tasks))
+                    { ?>
+
+                        <tr>
+                            <td> <?php echo $row['title'] ?></td>
+                            <td> <?php echo $row['description'] ?></td>
+                            <td> <?php echo $row['created_at'] ?></td>
+                        </tr>
+
+                    <?php } ?>
+                </tbody>
+            </table>
         </div>
     </div>
 </div>
