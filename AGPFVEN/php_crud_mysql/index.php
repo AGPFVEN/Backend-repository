132af9841a48af3e9335c8993209e2fbb2cd3c04
AGPFVEN <50179663+AGPFVEN@users.noreply.github.com>
2020-07-01 21:16:19 +0200

    comments

--- AGPFVEN/php_crud_mysql/index.php
+++ AGPFVEN/php_crud_mysql/index.php
@@ -12,10 +12,12 @@
 
                 <!--Así puedes meter código html entre php-->
                 <div class="alert alert-<?=$_SESSION['message_type'];?> alert-dismissible fade show" role="alert">
+
                     <?= $_SESSION['message'] ?>
                     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
+
                 </div>
 
                 <!-- Borra los datos que hay dentro de session -->
@@ -23,20 +25,28 @@
             } ?>
 
             <div class="card card-body">
+
                 <form action="save_task.php" method="POST">
+
                     <div class="form-group">
                         <input type="text" name="title" class="form-control" placeholder="Task title" autofocus>
                     </div>
+
                     <div class="form-group">
                         <textarea name="description" rows="2" class="form-control"
                             placeholder="Task Description"></textarea>
                     </div>
+
                     <input type="submit" class="btn btn-success btn-block" name="save_task" value="Save Task">
                 </form>
+
             </div>
         </div>
+
         <div class="com-md-8">
+
             <table class="table table-bordered">
+
                 <thead>
                     <tr>
                         <th> Title </th>
@@ -45,23 +55,28 @@
                         <th> Actions</th>
                     </tr>
                 </thead>
+                
                 <tbody>
                     <?php
                     $query = "SELECT * FROM task";                                       #Seleccionar todo dentro de la base de datos
-                    $result_tasks = mysqli_query($connection, $query);
+                    $result_tasks = mysqli_query($connection, $query);                   #Quary de MySQL (solicitud  a la base de datos)
 
-                    while($row = mysqli_fetch_array($result_tasks))
-                    { ?>
+                    while($row = mysqli_fetch_array($result_tasks))                      #while (Row va a ser un array, contiene todas las columnas y v)
+                    { ?>                                                                 <!-- Row va a ser false si se acaban las filas -->
 
                         <tr>
                             <td> <?php echo $row['title'] ?></td>
                             <td> <?php echo $row['description'] ?></td>
                             <td> <?php echo $row['created_at'] ?></td>
+                            <td> </td>
                         </tr>
 
                     <?php } ?>
+
                 </tbody>
+
             </table>
+
         </div>
     </div>
 </div>
