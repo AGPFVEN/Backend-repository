5624324aafab74f461fe4abde6e627170608dc92
AGPFVEN <50179663+AGPFVEN@users.noreply.github.com>
2020-07-10 22:39:21 +0200

    base of the new project

---
+++ AGPFVEN/BarCode/database/db.php
@@ -0,0 +1,9 @@
+<?php
+
+    session_start();
+    $connection = mysqli_connect(
+        'localhost',
+        'root',
+        '',
+        'php_mysql_crud_database'
+    );
---
+++ AGPFVEN/BarCode/index.php
@@ -0,0 +1,26 @@
+<?php include("database/db.php") ?>
+
+<?php include("includes/header.php")?>
+
+<div class="container p-4">
+    <div class="row">
+        <div class="col-md-4">
+
+
+
+            <div class="card card-body">
+
+                <form action="save_excel.php" method="POST">
+
+                    <div class="form-group">
+                        <input type="file" name="file" class="form-control">
+                    </div>
+
+                </form>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include("includes/footer.php")?>
