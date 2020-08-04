c9afd56174b8ca07c626514aa8f84dd0b767c2ba
AGPFVEN <50179663+AGPFVEN@users.noreply.github.com>
2020-08-05 00:51:22 +0200

    testing the new viewpoint

--- AGPFVEN/BarCode/checking_cables.php
+++ AGPFVEN/BarCode/checking_cables.php
@@ -1,5 +1,33 @@
 <?php include("db.php");
 
-echo 'bebeb';
+if(isset($_POST['Checking_cables']))
+{
+    if($_SESSION['n_cables'] == NULL)
+    {
+        $_SESSION['n_cables'] = $_POST['cable-name'];
+        $_SESSION['t_cables'] = 1;
+    }
+
+    echo $_SESSION['n_cables'];
+    echo '                   ';
+    echo $_SESSION['t_cables'];
+
+
+
+
+
+
+    $_SESSION['message'] = 'Your current order has been recalculated';
+    $_SESSION['message_type'] = 'success';
+
+    header("Location: index.php");
+}
+else
+{
+    $_SESSION['message'] = 'There is not a post';
+    $_SESSION['message_type'] = 'danger';
+
+    header("Location: index.php");
+}
 
 ?>
