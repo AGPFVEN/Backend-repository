13dab52d2754285c792077c9341b13c63e16175b
AGPFVEN <50179663+AGPFVEN@users.noreply.github.com>
2020-09-09 21:16:37 +0200

    enough testing

--- AGPFVEN/Amazon/test_folder/test_php.php
+++ AGPFVEN/Amazon/test_folder/test_php.php
@@ -23,17 +23,16 @@
     for($i = 0; $i < count($csvgroups);  $i++)
     {
         $checkpoint = 1;
-        $current_first_parentesis_pos = strpos($csvgroups[$i], '(', $checkpoint);
 
         //search parentesis
-        while($current_first_parentesis_pos != FALSE)
+        do
         {
             $current_first_parentesis_pos = strpos($csvgroups[$i], '(', $checkpoint);
             $current_second_parentesis_pos = strpos($csvgroups[$i], ')', $current_first_parentesis_pos);
             $current_comma_pos = strpos($csvgroups[$i], ',', $current_first_parentesis_pos);
 
             //change commas for dot comma
-            while($current_comma_pos < $current_second_parentesis_pos && $current_comma_pos > $current_first_parentesis_pos)
+            while($current_comma_pos < $current_second_parentesis_pos && $current_comma_pos > $current_first_parentesis_pos && $current_first_parentesis_pos != 0)
             {
                 $api_result_modifiedD = substr_replace($csvgroups[$i], '(', $current_first_parentesis_pos, 1);
                 $api_result_modified = substr_replace($api_result_modifiedD, ';', $current_comma_pos, 1);
@@ -46,7 +45,7 @@
             }
 
             $checkpoint = $current_first_parentesis_pos + 1;
-        }
+        } while($current_first_parentesis_pos != 0);
     }
 
 
