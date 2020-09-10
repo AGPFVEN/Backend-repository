24784a22d77628073b5756e09b0b1a850532aa8c
AGPFVEN <50179663+AGPFVEN@users.noreply.github.com>
2020-09-10 23:59:28 +0200

    update

--- AGPFVEN/Amazon/AGPFVEN-AMAZON.php
+++ AGPFVEN/Amazon/AGPFVEN-AMAZON.php
@@ -1,4 +1,11 @@
 <?php
+    //set up db
+    $connection = mysqli_connect(
+        'localhost',
+        'root',
+        '',
+        'php_mysql_crud_database'
+    );
 
     # set up the request parameters 
     $queryString = http_build_query([ 'api_key' => '757A4FBFF891445B9FEF6DE3441F190F', 'type' => 'search', 'amazon_domain' => 'amazon.com', 'search_term' => 'lc to lc fiber patch cable', 'output' => 'csv', 'customer_zipcode' => '33180', 'language' => 'en_US' ]); 
@@ -10,18 +17,28 @@
     $api_result = curl_exec($ch);
     
     # print the CSV response from Rainforest API
-    echo $api_result;
-    echo '-------------------------------------------------------------------------------------------------------------------------------------------------------';
+    // echo $api_result;
+    // echo '-------------------------------------------------------------------------------------------------------------------------------------------------------';
 
     // Erase quotes
     $csv_result_erased_1_2 = str_replace('"', '', $api_result);
     $csv_result_erased_2_2 = str_replace('"', '', $csv_result_erased_1_2);
 
+    // echo $csv_result_erased_2_2;
+
     //grups of lines
     $csvgroups = explode("\n", $csv_result_erased_2_2);
 
+    while(strpos($csvgroups[0], '.') != 0)
+    {
+        $csvgroups[0] = substr_replace($csvgroups[0], '_', strpos($csvgroups[0], '.'), 1);
+    }
+
+    echo $csvgroups[0];
+
+
     //search comma in array
-    for($i = 0; $i < count($csvgroups);  $i++)
+    for($i = 1; $i < count($csvgroups); $i++)
     {
         $checkpoint = 1;
 
@@ -47,16 +64,24 @@
 
             $checkpoint = $current_first_parentesis_pos + 1;
         } while($current_first_parentesis_pos != 0);
+
+        //Separete in words
+        $csvgroups_individual = explode(',', $csvgroups[$i]);
+
+        var_dump($csvgroups_individual);
+
+        $query = "INSERT INTO amazon_test (request_parameters_type, request_parameters_amazon_domain, request_parameters_search_term, request_parameters_sort_by, request_parameters_page, search_results_position, search_results_title, search_results_asin, search_results_link, search_results_image, search_results_rating, search_results_ratings_total, search_results_is_prime, search_results_sponsored, search_results_price_currency, search_results_price_value, search_results_page, search_results_add_on_item_is_add_on_item) VALUES ('$csvgroups_individual[0]', '$csvgroups_individual[1]', '$csvgroups_individual[2]', '$csvgroups_individual[3]', '$csvgroups_individual[4]', '$csvgroups_individual[5]', '$csvgroups_individual[6]', '$csvgroups_individual[7]', '$csvgroups_individual[8]', '$csvgroups_individual[9]', '$csvgroups_individual[10]', '$csvgroups_individual[11]', '$csvgroups_individual[12]', '$csvgroups_individual[13]', '$csvgroups_individual[14]', '$csvgroups_individual[15]', '$csvgroups_individual[16]', '$csvgroups_individual[17]'";
+        $result = mysqli_query($connection, $query);
     }
 
-    // $gestor = fopen('D:\xampp-Server\htdocs\Backend-repository\AGPFVEN\Amazon\AGPFVEN-AMAZON-CSV.CSV', 'w');
+//     // $gestor = fopen('D:\xampp-Server\htdocs\Backend-repository\AGPFVEN\Amazon\AGPFVEN-AMAZON-CSV.CSV', 'w');
 
-    // fputcsv($gestor, $csv_result);
+//     // fputcsv($gestor, $csv_result);
 
-    // fclose($gestor);
+//     // fclose($gestor);
     
-    echo "\n";
+//     // echo "\n";
 
-    //See variables
-    var_dump($csvgroups);
-?>
+//     //See variables
+//     // var_dump($csvgroups);
+// ?>
