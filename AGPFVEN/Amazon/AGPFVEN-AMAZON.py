991de0a231f8b465b25f8cfa9372c7e3bd36bac6
AGPFVEN <50179663+AGPFVEN@users.noreply.github.com>
2020-12-05 23:51:28 +0100

    new comission

--- AGPFVEN/Amazon/AGPFVEN-AMAZON.py
+++
@@ -1,5 +0,0 @@
-import sys
-
-print("hola a tpdps")
-
-# print(sys.argv[1])
---
+++ AGPFVEN/Made_for_Luis/Made_for_luis.php
@@ -0,0 +1,11 @@
+<?php
+
+    //set up db, studying consumes a lot of time
+    $connection = mysqli_connect(
+        'localhost',
+        'root',
+        '',
+        'php_mysql_crud_database'
+    );
+
+?>
