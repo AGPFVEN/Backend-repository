8a4f184e5bac5a74dcdf5e58f0c3de89240081ce
AGPFVEN <50179663+AGPFVEN@users.noreply.github.com>
2021-01-09 03:27:16 +0100

    prototype

--- AGPFVEN/FSNKU_crisis/execute_python_.py
+++ AGPFVEN/FSNKU_crisis/execute_python_.py
@@ -1,5 +1,7 @@
 import os
 import time
 
+os.system('cmd /k "D:"')
 while True:
-    os.system('cmd /k "Your Command Prompt Command"')
+    os.system('cmd /k "python D:\xampp\htdocs\xampp\Backend-repository\AGPFVEN\FSNKU_crisis\python_test\print_test.py"')
+    time.sleep(5)
