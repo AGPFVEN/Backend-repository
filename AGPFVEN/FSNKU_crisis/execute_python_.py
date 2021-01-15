4ce86df69233df2bc25841b90c6580469b7590bc
AGPFVEN <50179663+AGPFVEN@users.noreply.github.com>
2021-01-15 19:15:29 +0100

    new idea

--- AGPFVEN/FSNKU_crisis/execute_python_.py
+++
@@ -1,9 +0,0 @@
-import os
-import time
-
-# os.system('cmd /k "D:"')
-while True:
-    # os.system('cmd /k "python"')
-    # os.system('cmd /k "D:\\xampp\\htdocs\\xampp\\Backend-repository\\AGPFVEN\\FSNKU_crisis\\python_test\\print_test.py"')
-    
-    time.sleep(5)
--- AGPFVEN/FSNKU_crisis/test_folder/Use_printer.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-    $handle = printer_open("Brother DCP-J562DW Pinter");
-
-?>
