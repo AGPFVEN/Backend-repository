2e0fce2164246cd46fa5c7a781b4b0356beb6c99
AGPFVEN <50179663+AGPFVEN@users.noreply.github.com>
2021-01-17 19:17:09 +0100

    py done

---
+++ AGPFVEN/FSNKU_crisis/db_result_python.py
@@ -0,0 +1,3 @@
+import win32api
+file_path = open("File_path.txt", "r")
+win32api.ShellExecute(0, "print", file_path.read(), None, ".", 0)
---
+++ AGPFVEN/FSNKU_crisis/python_executer_db.py
@@ -0,0 +1,5 @@
+import time
+
+while True:
+    exec(open("D:\\xampp\\htdocs\\xampp\\Backend-repository\\AGPFVEN\\FSNKU_crisis\\print_test.py").read())        
+    time.sleep(5)
